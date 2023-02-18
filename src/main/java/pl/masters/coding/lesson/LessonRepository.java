ce7fc9035aac4539088b189aedfc193f52b3dcb9
pJona1337 <allsti@wp.pl>
2023-02-18 13:02:34 +0100

    teacher list working

--- src/main/java/pl/masters/coding/lesson/LessonRepository.java
+++ src/main/java/pl/masters/coding/lesson/LessonRepository.java
@@ -1,10 +1,12 @@
 package pl.masters.coding.lesson;
 
+import org.springframework.stereotype.Repository;
 import pl.masters.coding.lesson.model.Lesson;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class LessonRepository {
 
     private static final List<Lesson> dummyLessonsDb = new ArrayList<>();
--- src/main/java/pl/masters/coding/teacher/TeacherController.java
+++ src/main/java/pl/masters/coding/teacher/TeacherController.java
@@ -2,7 +2,9 @@ package pl.masters.coding.teacher;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,7 +15,12 @@ public class TeacherController {
     private final TeacherService teacherService;
 
     @GetMapping
-    public String getTeacherList() {
+    public String getTeacherList(Model model) {
+        model.addAttribute("teachers", teacherService.findAll());
         return "teacher/list";
     }
+
+//    @GetMapping("/create")
+//
+//    @PostMapping("/create")
 }
