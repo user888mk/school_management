0b5cf23c341ef5d94becb407d3bafebef1abdfe7
user888mk <marcin.krysiak889@gmail.com>
2023-02-18 11:58:37 +0100

    lesson model added

---
+++ src/main/java/pl/masters/coding/lesson/LessonController.java
@@ -0,0 +1,21 @@
+package pl.masters.coding.lesson;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.masters.coding.lesson.model.Lesson;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/lesson")
+public class LessonController {
+
+    private LessonService lessonService;
+
+    @GetMapping
+    public String getStudentList(){
+        return "lesson/list";
+    }
+
+}
---
+++ src/main/java/pl/masters/coding/lesson/model/Lesson.java
@@ -0,0 +1,23 @@
+package pl.masters.coding.lesson.model;
+
+import lombok.*;
+import pl.masters.coding.student.model.Student;
+import pl.masters.coding.teacher.model.Teacher;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Lesson {
+
+    private Long id;
+
+    private Student student;
+
+    private Teacher teacher;
+
+    private LocalDate date;
+
+}
